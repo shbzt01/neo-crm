db3273fb151c84d239f3c8bef7df0a0f46845056
BAIRQ_NEO <neo@bairq.com>
2024-04-04 06:42:34 +0000

    4496-rebuild-admin-dashboard-layout

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -1,50 +1,62 @@
-@extends('layouts.app')
-
-@section('sidebar')
-<div class="sidebar bg-light border-right">
-    <ul class="nav flex-column">
-        <li class="nav-item">
-            <a class="nav-link active" href="/dashboard">
-                <i class="fas fa-home"></i> Dashboard
-            </a>
-        </li>
-        <!-- Additional links can be added here -->
-    </ul>
-</div>
-@endsection
-
-@section('header')
-<nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom">
-    <button class="btn btn-primary" id="menu-toggle">Toggle Menu</button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    User Settings
-                </a>
-                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                    <a class="dropdown-item" href="#">Profile</a>
-                    <a class="dropdown-item" href="#">Settings</a>
-                    <div class="dropdown-divider"></div>
-                    <a class="dropdown-item" href="#">Logout</a>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>NEO CRM Dashboard</title>
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <style>
+        .sidebar {
+            min-height: 100vh;
+        }
+        .footer {
+            background-color: #f8f9fa;
+            text-align: center;
+            padding: 10px 0;
+        }
+    </style>
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">NEO CRM</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Features</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Pricing</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-2 sidebar bg-light">
+                <div class="list-group">
+                    <a href="#" class="list-group-item list-group-item-action active">Dashboard</a>
+                    <a href="#" class="list-group-item list-group-item-action">Users</a>
+                    <a href="#" class="list-group-item list-group-item-action">Settings</a>
                 </div>
-            </li>
-        </ul>
+            </div>
+            <div class="col-md-10">
+                <div class="container">
+                    @yield('content')
+                </div>
+            </div>
+        </div>
     </div>
-</nav>
-@endsection
-
-@section('content')
-<div class="responsive-content container-fluid">
-    <!-- Main content here -->
-</div>
-@endsection
-
-@push('scripts')
-<script>
-    $('#menu-toggle').click(function(e) {
-        e.preventDefault();
-        $('.wrapper').toggleClass('toggled');
-    });
-</script>
-@endpush
+    <div class="footer">
+        <p>&copy; 2023 NEO CRM. All rights reserved.</p>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.2/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>
