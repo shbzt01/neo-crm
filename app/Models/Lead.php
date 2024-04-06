da555477a484a52d4ce041fe37898f1720a5a954
suhaib <suhaib@bairq.com>
2024-04-06 09:32:05 +0300

    lead updates small changes

--- app/Models/Lead.php
+++ app/Models/Lead.php
@@ -7,7 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class Lead extends Model
 {
     protected $table = 'leads';
-
+    protected $fillable = [
+        'customer_id',
+        'contact_details',
+        'source',
+        'initial_notes',
+        'status'
+    ];
     public function customer()
     {
         return $this->belongsTo(Customer::class, 'customer_id');
