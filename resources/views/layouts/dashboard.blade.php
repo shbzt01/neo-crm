c29a852614ed46b557ca58482a31bc601195ff21
BAIRQ_NEO <neo@bairq.com>
2024-09-29 11:04:21 +0000

    2247-convert-the-footer-to-2024

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -70,7 +70,7 @@
         </div>
     </div>
     <div class="footer">
-        <p>&copy; 2023 NEO CRM. All rights reserved.</p>
+        <p>&copy; 2024 NEO CRM. All rights reserved.</p>
     </div>
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.2/dist/umd/popper.min.js"></script>
